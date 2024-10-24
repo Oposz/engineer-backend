0b78300891938c74893c4d0da065965ff99ebedb
Przemek Gulik <gulikdev@gmail.com>
2024-10-24 22:04:04 +0200

    code cleanup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '../auth/auth.guard';
 import { UsersService } from './users.service';
 import { User } from '../decorators/user.decorator';
